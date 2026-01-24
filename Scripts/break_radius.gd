f7ea12098cf6369c9a247bfa67deabc5242d25ab
tylerJordan223 <tjlinkmaster22@gmail.com>
2026-01-24 01:51:31 -0500

    Re-Did tile collision
    Now every tile within the break_radius collision body will be damaged overtime.

--- Scripts/break_radius.gd
+++ Scripts/break_radius.gd
@@ -21,59 +21,31 @@ func _physics_process(_delta: float):
 	if has_overlapping_bodies():
 		var overlaps = get_overlapping_bodies()
 		for o in overlaps:
+			#means its the terrain
 			if o.is_in_group("Breakable"):
-				var collision_shape = $CollisionShape2D
-				var size = collision_shape.shape.size
-				
 				#reset the list of damaged tiles this frame
 				tiles_this_frame = []
 				
-				#check 4 edges and middle#
-				#middle
-				collision_at_position(global_position)
-				#left
-				collision_at_position(Vector2(global_position.x - size.x/2, global_position.y))
-				#right
-				collision_at_position(Vector2(global_position.x + size.x/2, global_position.y))
-				#top
-				collision_at_position(Vector2(global_position.x, global_position.y - size.y/2))
-				#bottom
-				collision_at_position(Vector2(global_position.x, global_position.y + size.y/2))
+				#run the collision
+				dig_tiles(o, global_position)
 
 #checks the overlap for a breakable layer
-func dig_tile(object, pos):
-	var local_pos = object.to_local(pos)
-	var tile_pos = object.local_to_map(local_pos)
-	#only do it if it has not been damaged this frame
-	if !tiles_this_frame.has(tile_pos):
-		object.get_parent().grid[tile_pos.y][tile_pos.x].dig()
-		#add it to the list for this frame
-		tiles_this_frame.append(tile_pos)
-
-func collision_at_position(p: Vector2):
-	var state_space = get_world_2d().direct_space_state
+func dig_tiles(object, pos):
+	#get the 4 corners to work with
+	var x_min = floor(global_position.x - $CollisionShape2D.shape.size.x/2)
+	var x_max = ceil(global_position.x + $CollisionShape2D.shape.size.x/2)
+	var y_min = floor(global_position.y - $CollisionShape2D.shape.size.y/2)
+	var y_max = ceil(global_position.y + $CollisionShape2D.shape.size.y/2)
 	
-	#set up the query
-	var q = PhysicsPointQueryParameters2D.new()
-	q.position = p
-	q.collide_with_areas = false
-	q.collide_with_bodies = true
+	#translate them to local
+	var min = object.local_to_map(object.to_local(Vector2(x_min,y_min)))
+	var max = object.local_to_map(object.to_local(Vector2(x_max,y_max)))
 	
-	#get all nodes that arent the tilemap to exclude them
-	var exclude = []
-	for node in get_tree().get_nodes_in_group("Breakable"):
-		if node is CollisionObject2D:
-			exclude.append(node.get_rid())
-	q.exclude = exclude
-	
-	#should get just the tilemap if anything
-	var result = state_space.intersect_point(q)
-	
-	#makes sure its a tilemap and exists
-	if result.size() > 0:
-		#only run it if it finds the tilemap at the position
-		var tilemap = result.get(0).get("collider")
-		if tilemap is TileMapLayer:
-			dig_tile(tilemap, p)
-	
-	pass
+	for x in range(min.x, max.x + 1):
+		for y in range(min.y, max.y + 1):
+			#only do it if it has not been damaged this frame
+			if !tiles_this_frame.has(Vector2i(x,y)):
+				if x - object.get_parent().grid[0].size() < 0 and y - object.get_parent().grid.size() < 0:
+					object.get_parent().grid[y][x].dig()
+					#add it to the list for this frame
+					tiles_this_frame.append(Vector2i(x,y))
