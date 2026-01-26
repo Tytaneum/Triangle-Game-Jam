72e9549036a2f93752f2ea2cc87e25c18520b966
tylerJordan223 <tjlinkmaster22@gmail.com>
2026-01-26 03:34:44 -0500

    Fixed the timer

--- Scripts/time.gd
+++ Scripts/time.gd
@@ -1,11 +1,26 @@
 extends Node2D
 
+var done
+
 func _ready():
 	await get_tree().create_timer(.3).timeout
 	$TextureProgressBar.max_value = $Timer.wait_time
 	Global.TIME = $Timer.wait_time
+	done = false;
 
 func _physics_process(_delta: float):
-	Global.TIME -= _delta
-	$Timer.wait_time = Global.TIME
-	$TextureProgressBar.value = Global.TIME
+	if Global.TIME > 0:
+		Global.TIME -= _delta
+		$Timer.wait_time = Global.TIME
+		$TextureProgressBar.value = Global.TIME
+	else:
+		if $Timer.wait_time != 0:
+			$Timer.wait_time = 0
+			$TextureProgressBar.value = 0
+		if !done:
+			finish()
+
+func finish():
+	done = true
+	$Timer.wait_time = 1
+	$Timer.start()
