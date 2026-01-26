993247260db92fb2d169de7e40920357c721bc24
Tytaneum <152898694+Tytaneum@users.noreply.github.com>
2026-01-26 08:37:35 -0500

    added transition from title screen

--- Scenes/title_screen.gd
+++ Scenes/title_screen.gd
@@ -5,6 +5,7 @@ func _ready() -> void:
 	$AnimationPlayer.queue("title oscilate")
 	$"Drill Selector".play()
 	$"Start Button".grab_focus()
+	$StartTransition.visible = false
 
 func _physics_process(delta: float) -> void:
 	if $"Start Button".has_focus():
@@ -15,8 +16,17 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+	$"UI AnimationPlayer".play("start select")
 
 
 func _on_controls_button_pressed() -> void:
+	$"UI AnimationPlayer".play("controls select")
 	pass #idk do controls menu stuff
+
+
+func _on_ui_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "start select":
+		$StartTransition.visible = true
+		$StartTransition/AnimationPlayer.play("closing_transition")
+		await  $StartTransition/AnimationPlayer.animation_finished
+		get_tree().change_scene_to_file("res://Scenes/main.tscn")
