6a411276419f5dbf69d207b10731ba51117a9c80
tylerJordan223 <tjlinkmaster22@gmail.com>
2026-01-26 02:13:12 -0500

    Applied Blast Rock Texture + Changed Time
    Time now uses the Global.TIME value, which is decreased by delta every frame.

--- Scripts/time.gd
+++ Scripts/time.gd
@@ -3,7 +3,9 @@ extends Node2D
 func _ready():
 	await get_tree().create_timer(.3).timeout
 	$TextureProgressBar.max_value = $Timer.wait_time
-	print($Timer.wait_time)
+	Global.TIME = $Timer.wait_time
 
 func _physics_process(_delta: float):
-	$TextureProgressBar.value = $Timer.time_left
+	Global.TIME -= _delta
+	$Timer.wait_time = Global.TIME
+	$TextureProgressBar.value = Global.TIME
