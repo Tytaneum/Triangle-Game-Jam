03b0de7295a3cb614eac600a8d58cc8de5f752df
Nuno <61714135+NunoR1@users.noreply.github.com>
2026-01-26 13:50:23 -0500

    main.gd

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -7,13 +7,12 @@ func _ready():
 	game_time.start(Global.TIME)
 
 func _physics_process(_delta: float):
-	print(Global.current_depth)
 	if int(game_time.time_left) == 0:
 		game_time.stop()
 		Global.cutscene = true
 		Global.lose.emit()
 		print("Game Over")
-		
+
 	if Global.current_depth == Global.FINAL_DEPTH:
 		game_time.stop()
 		Global.cutscene = true
